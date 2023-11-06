5f7162f25a3992a8ae0061db116deba23bed6cbf
jishrami <jramirez@byui.edu>
2023-11-06 10:50:50 -0700

    week 2 tasks

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -9,23 +9,35 @@ let profilePicture = 'images/me-in-costume.jpg';
 
 
 /* Step 3 - Element Variables */
-const nameElement = document.getElementsById('name');
+const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
 
 
 
 /* Step 4 - Adding Content */
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute ( 'src', profilePicture);
+imageElement.setAttribute ('alt', `Profile image of ${fullName}`);
 
 
 
 
 /* Step 5 - Array */
+let foods = ["spaghetti", "Taco Bell", "hot dogs", "ice cream"];
+foodElement.innerHTML = foods;
+let newFood = "chocolate milk";
 
+foods.push(newFood);
 
+foodElement.innerHTML += `<br>${foods}`;
 
+foods.shift();
 
+foodElement.innerHTML += `<br>${foods}`;
 
+foods.pop();
 
+foodElement.innerHTML += `<br>${foods}`;
